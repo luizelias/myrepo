1579d6933a794bac49828cfd0500a7b793c87c5f
Luiz Elias <luiz.elias1981@gmail.com>
2023-10-31 23:33:58 -0300

    including glm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#include <glm/glm.hpp>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World 22!" << std::endl;
+  glm::vec3 v1(1.0f, 0.0f, 0.0f);
+  glm::vec3 v2(0.5f, 1.0f, 0.0f);
+
+  auto v3 = v1 + v2;
+
+  std::cout << "vector.x = " << v3.x << std::endl;
 
   return 0;
 }
