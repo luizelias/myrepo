e28ba23818fd2439be85090065d4e310d673f176
luizelias <31769419+luizelias@users.noreply.github.com>
2023-11-03 10:52:04 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include <glm/glm.hpp>
+//#include <glm/glm.hpp>
 #include <iostream>
 
 int main() {
-  glm::vec3 v1(1.0f, 0.0f, 0.0f);
-  glm::vec3 v2(0.5f, 1.0f, 0.0f);
+  //glm::vec3 v1(1.0f, 0.0f, 0.0f);
+  //glm::vec3 v2(0.5f, 1.0f, 0.0f);
 
-  auto v3 = v1 + v2;
+ // auto v3 = v1 + v2;
 
-  std::cout << "vector.x = " << v3.x << std::endl;
+ // std::cout << "vector.x = " << v3.x << std::endl;
 
   return 0;
 }
