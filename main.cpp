7ac1e8ffacdbe1ae6957c4947e865ea7307957aa
luizelias <31769419+luizelias@users.noreply.github.com>
2023-11-10 17:44:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,73 @@
-//#include <glm/glm.hpp>
+#include <nlohmann/json.hpp>
+#include <Eigen/Dense>
+#include <alembic/AbcGeom/All.h>
+#include <glm/glm.hpp>
+#include <fmt/core.h>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <asio.hpp>
+
 #include <iostream>
 
 int main() {
-  //glm::vec3 v1(1.0f, 0.0f, 0.0f);
-  //glm::vec3 v2(0.5f, 1.0f, 0.0f);
+    // Example usage of nlohmann_json
+    nlohmann::json jsonExample;
+    jsonExample["message"] = "Hello, world!";
 
- // auto v3 = v1 + v2;
+    // Example usage of Eigen
+    Eigen::MatrixXd matrixExample(2, 2);
+    matrixExample(0, 0) = 3;
+    matrixExample(1, 0) = 2.5;
+    matrixExample(0, 1) = -1;
+    matrixExample(1, 1) = matrixExample(1, 0) + matrixExample(0, 1);
 
- // std::cout << "vector.x = " << v3.x << std::endl; 
+    // Example usage of Alembic (just a dummy call for demonstration)
+    Alembic::AbcGeom::ALEMBIC_VERSION_NS::GetLibraryVersion();
 
-  return 0;
+    // Example usage of glm
+    glm::vec3 vecExample(1.0f, 0.0f, 0.0f);
+
+    // Example usage of fmt
+    fmt::print("Formatted text from fmt: {}\n", jsonExample.dump());
+
+    // GLFW and Glad example (Creating a window)
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Load GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Main render loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    // Example usage of Asio (just a version print for demonstration)
+    std::cout << "ASIO version: " << ASIO_VERSION << std::endl;
+
+    // Printing Eigen matrix
+    std::cout << "Here is an Eigen matrix:\n" << matrixExample << std::endl;
+
+    // Final message print
+    std::cout << "Final message: " << jsonExample["message"] << std::endl;
+
+    return 0;
 }
